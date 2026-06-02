0f415edbbced114ebe0035d863f588c188e52b25
Những đứa trẻ tăng động nhìn xa trông rộng thấy những bóng hồng đang chờ mong ngóng chồng <ngodlamm@gmail.com>
2026-06-02 21:08:23 +0700

    Add files via upload

---
+++ 3/Bai1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int gcd(int a, int b){
+    if (b == 0){
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int g = gcd(a, b);
+    printf("%d \n", g);
+    printf("%d", a * b / g);
+    return 0;
+}
